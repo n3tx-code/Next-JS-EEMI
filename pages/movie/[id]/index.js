67f9cf2bc043f8987402589e21f9e0f54faadc87
Nils <nils@vaede.com>
2022-06-09 15:53:12 +0200

    feat: design for movie detail page and responsive design

--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { motion } from "framer-motion"
 
-
 export async function getStaticPaths() {
   const url = "https://api.themoviedb.org/3/discover/movie?api_key=5816d3c43849695433b83810de80eccc&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
   const { data } = await axios.get(url);
-  console.log(data);
   return {
     paths: data.results.map(({ id }) => `/movie/${id}`),
     fallback: false
@@ -21,17 +19,27 @@ export async function getStaticProps({ params }) {
 
   return {
     props: {
-      movie: data
+      movie: data,
     }
   };
 }
 
 export default function Card({ movie }) {
-  console.log(movie);
-
   return (
-    <div>
-      <h2>{movie.original_title}</h2>
+    <div className="movieWrapper">
+      <motion.div animate={{ x: 0 }} initial={{ x: -700 }} transition={{ type: "spring", stiffness: 100 }} className="movieImageWrapper">
+        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.original_title} className="movieImage" />
+      </motion.div>
+      <motion.div animate={{ x: 0 }} initial={{ x: 700 }} transition={{ type: "spring", stiffness: 100 }} className="movieDetailWrapper">
+        <h1>{movie.original_title}</h1>
+        {movie.genres.map(({ name }) => (
+          <span key={movie.original_title + name} className="movieGenre">{name}</span>
+        ))}
+        <span className={`movieScore ${movie.vote_average > 7 ? "goodScore" : "badScore"}`}>{movie.vote_average}</span>
+        <p className="movieOverview">
+          {movie.overview}
+        </p>
+      </motion.div>
     </div>
   );
 }
--- styles/global.js
+++
@@ -1,9 +0,0 @@
-import css from 'styled-jsx/css';
-
-export default css.global`
-  body {
-    margin: 0;
-    background-color: black;
-    color: #fff; 
-  }
-`;
